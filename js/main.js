71037f27364d7906f9454dca77fbc5cc45ac4a00
Renato15767 <renatominoru46@gmail.com>
2023-03-05 20:39:05 -0300

    Item fica salvo na página

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
 //Cria um vetor vazio caso não tiver nada no localStorage
-const itens = localStorage.getItem("itens") || []
-console.log(itens)
+//O "JSON" transforma de string para um objeto
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+//Irá percorrer o array
+itens.forEach(elemento => {
+    criaElemento(elemento)
+});
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-
+    
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(nome.value, quantidade.value)
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    criaElemento(itemAtual)
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 
     nome.value = ""
     quantidade.value = ""
 })
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
     //<li class="item"><strong>7</strong>Camisas</li>
     //Cria a tag <li>
     const novoItem = document.createElement('li')
@@ -26,20 +40,14 @@ function criaElemento(nome, quantidade) {
     //Cria a tag <strong>
     const numeroItem = document.createElement('strong')
     //Adiciona o valor "quantidade"
-    numeroItem.innerHTML = quantidade
+    numeroItem.innerHTML = item.quantidade
     //Adiciona o "numeroItem" dentro do "novoItem"
     novoItem.appendChild(numeroItem)
     
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += item.nome
 
+    //Adiciona no HTML
     lista.appendChild(novoItem)
 
-    const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade
-    }
-
-    itens.push(itemAtual)
-
-    localStorage.setItem("itens", JSON.stringify(itens))
 }
+
